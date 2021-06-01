cf9e6a0cf5562caeb558d392830549750a992ed0
humanrizki <humanrizki123@gmail.com>
2021-06-01 16:50:29 +0700

    baru tahap controller

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php 
+
+class About{
+    public function index($nama = 'RIZKI', $pekerjaan = 'Programmer'){
+        echo "HI, nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page(){
+        echo "About/page";
+    }
+}
