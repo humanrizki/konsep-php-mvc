f031fc3b129886c821d91c38414f87205ff1d4a9
humanrizki <humanrizki123@gmail.com>
2021-06-04 20:49:48 +0700

    config dan wrapping database

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -4,11 +4,10 @@
             <h3>Daftar Mahasiswa</h3>
             
                 <?php foreach($data['mhs'] as $mhs):?>
-                <ul>
-                    <li><?= $mhs['nama'];?></li>
-                    <li><?= $mhs['nis'];?></li>
-                    <li><?= $mhs['kelas'];?></li>
-                </ul>
+                    <ul class="list-group py-2">
+
+                        <li class="list-group-item d-flex justify-content-between align-items-center shadow"><?= $mhs['nama'];?> <a href="<?= BASEURL;?>/mahasiswa/detail/<?= $mhs['id'];?>" class="btn btn-primary ">detail</a></li>
+                    </ul>
                 <?php endforeach;?>
             
         </div>
